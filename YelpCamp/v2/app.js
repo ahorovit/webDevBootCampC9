45ae0f3c86a17eb8b9246a602baa6187e4a96225
Adin H <ahorovit@gmail.com>
2019-04-11 02:23:23 +0000

    Mongo persistence introduced

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -1,40 +1,79 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "Salmon Creek", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-blm.jpg"},
-    {name: "Walnut Brook", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-nationalpark.jpg"},
-    {name: "Bullshit Peak", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-commercial.jpg"},
-    {name: "Salmon Creek", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-blm.jpg"},
-    {name: "Walnut Brook", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-nationalpark.jpg"},
-    {name: "Bullshit Peak", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-commercial.jpg"}
-];
-
-app.get("/", function(req, res){
-   res.render("landing"); 
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
 });
 
-app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds:campgrounds});
-});
+var Campground = mongoose.model("Campground", campgroundSchema);
 
-app.get("/campgrounds/new", function(req, res){
-   res.render("new");
-});
 
-app.post('/campgrounds', function(req, res){
-    // get data from form and add to campgorunds array
-    campgrounds.push({name: req.body.name, image: req.body.image});
+// Campground.create({
+//         name: "Walnut Brook",
+//         image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-nationalpark.jpg"
     
-    // redirect back to campgrounds page (get)
-    res.redirect("/campgrounds");
+//     }, function(err, campground) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         else {
+//             console.log(campground);
+//         }
+// });
+
+
+
+// var campgrounds = [
+//     { name: "Salmon Creek", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-blm.jpg" },
+//     { name: "Walnut Brook", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-nationalpark.jpg" },
+//     { name: "Bullshit Peak", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-commercial.jpg" },
+//     { name: "Salmon Creek", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-blm.jpg" },
+//     { name: "Walnut Brook", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-nationalpark.jpg" },
+//     { name: "Bullshit Peak", image: "https://www.discovermoab.com/wp-content/uploads/2017/10/camping-commercial.jpg" }
+// ];
+
+app.get("/", function(req, res) {
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+    // Get all campgrounds
+    Campground.find({}, function(err,allCampgrounds){
+       if (err) {
+           console.log(err);
+       } else {
+            res.render("campgrounds", { campgrounds: allCampgrounds });
+       }
+    });
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new");
+});
+
+app.post('/campgrounds', function(req, res) {
+    // get data from form and add to campgorunds array
+    Campground.create({ 
+        name: req.body.name, 
+        image: req.body.image 
+    }, function(err, newCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("YelpCamp Server Has Started!");
 });
