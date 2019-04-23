05dda9fb2e641b8ac292c843fbbe5dfdae9da0e0
Adin H <ahorovit@gmail.com>
2019-04-23 01:51:02 +0000

    style show page

--- YelpCamp/v5/app.js
+++ YelpCamp/v5/app.js
@@ -9,6 +9,7 @@ var express = require("express"),
 mongoose.connect('mongodb://localhost:27017/yelp_camp_v3', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 // Clear Campgrounds and generate seed data
 seedDB();
